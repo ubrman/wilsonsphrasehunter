2c41b93efabf97389ce607a28c232087c403a4b7
ubrman <35584635+ubrman@users.noreply.github.com>
2020-10-17 21:17:48 -0600

    Update phrase.py

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,26 @@
 
+class Phrase:
+    def __init__(self, wisdom):
+        self.wisdoms = wisdom.lower()
+    
+    def display(self, guesses):
+        for letters in self.wisdoms:
+            if letters in guesses:
+                print(f"{letters}", end=" ")
+            else: 
+                print("_", end=" ")
+        print("\n")
+    
+    def check_guess(self, guess):
+        if guess in self.wisdoms:
+            return True
+        else:
+            return False
+
+    def check_complete(self, guesses):
+        for letter in self.wisdoms:
+            if letter in guesses:
+                continue
+            else:
+                return False
+        return True
